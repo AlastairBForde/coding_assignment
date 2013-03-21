4f5b0ede415e7c704169ab27f5fd4e9a3d823584
AlastairBForde <alastair.forde@gmail.com>
2013-03-21 08:44:27 -0400

    Added in FlitterTweeter

---
+++ FlitterTweeter/FlitterTweeter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Diagnostics;
+using TweetSharp;
+using TweetSharp.Model;
+
+namespace FlitterTweeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This app's unique Consumer Key (first argument) and Consumer Secret (second argument)
+            TwitterService t_serv = new TwitterService("agvQyQIx2FFciJSpgCSwQ", "vdmcbG4EHveIQPA65WWzW1KYRFFFZY2B7MWFf2BHXqc");
+
+            // Use this app's consumer info (consumer key and consumer secret) to get request token
+            OAuthRequestToken r_token = t_serv.GetRequestToken();
+            
+            // Use request token and send user to Twitter to get a PIN
+            Uri uri_1 = t_serv.GetAuthorizationUri(r_token); // get authorization URI using request token
+            Process.Start(uri_1.ToString()); // 
+
+            // Take in PIN and generate an access token and secret
+            Console.WriteLine("Please enter the PIN number you received from Twitter: ");
+            string ver_num = Console.ReadLine();
+            OAuthAccessToken a_token = t_serv.GetAccessToken(r_token, ver_num);
+
+            // Console.WriteLine("The PIN you entered was " + ver_num + ".");
+            // Console.ReadKey(true);
+
+            // authenticate using the access token and secret
+            t_serv.AuthenticateWith(a_token.Token, a_token.TokenSecret);
+
+            IEnumerable<TwitterStatus> mentions = t_serv.ListTweetsMentioningMe();
+            // step 4: save user data in a database
+
+            // step 5: display database info in a webpage
+            
+        }
+    }
+}
